c06ab3f688110882d99ab2053af0be3997b1b41b
kairya <kariya@kariya.cc>
2013-11-12 15:59:04 +0900

    new sample

---
+++ sample/x64.asm
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+char *code, *codep;	
+
+/* initialize; call first once */
+void emitInit() {
+	int psize = sysconf(_SC_PAGESIZE);
+	if(posix_memalign(&code, psize, psize)) err(1, "memalign");
+	if (mprotect(code, psize, PROT_EXEC|PROT_READ|PROT_WRITE)) err(1, "mprotect");
+	codep = code;
+}
+
+/* emits machine codes */
+char* emit(int argc, ...) {
+	int i;
+	char* start = codep;
+
+	va_list argp;
+	va_start(argp, argc);
+	for (i = 0; i < argc; ++i) {
+		char op = va_arg(argp, int);
+		*codep++ = op;
+	}
+	va_end(argp);
+	return start;
+}
+
+/* returns address which is the start address of next emit() */
+char* emitPc() { return codep; }
+
+/* function prologue */
+char* emitPrologue() {
+	char* start = emit([[nop]]);
+	return start;
+}
+
+/* function epilogue */
+char* emitEpilogue() {
+	char* start = emit([[ret]]);
+	return start;
+}
+
+/* debug utility */
+void dump() {
+	int i;
+	printf("int f(int x) {");
+	printf("\tasm(\".org 0x%x\");\n", (int) code);
+	for (i = 0; i < emitPc() - code; ++i) printf("\tasm(\".byte 0x%x\");\n", code[i] & 0xff);
+	printf("}");
+}
+
+/* int f(int x, int y) { return x + y; } */
+main () {
+	emitInit();
+
+	char* f = emitPrologue();
+	emit([[add %rdi, %rsi]]);
+	emit([[mov %rsi, %rax]]);
+	emitEpilogue();
+
+#ifdef DUMP
+	dump();
+#else
+	int x = ((int (*)(int, int))f)(1, 2);
+	printf("%d\n", x);
+#endif
+}
+
